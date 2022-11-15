8224fe06b2d0146374a2dd5fc5f5c4a97d58a338
kaiinaba <devil_orebaka@hotmail.com>
2022-11-15 21:26:53 +0900

    update

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ dt_yesterday = dt_now - td
 dt_yesterday = str(dt_yesterday)
 dt_yesterday = dt_yesterday[0:10]
 
-with open('file.txt', 'w') as f:
-    print(dt_yesterday + "<br>", file=f)
-    print("--------------------------------" + "<br>", file=f)
-    with open('sites.csv', 'r') as file:
-        reader = csv.reader(file)
+print(dt_yesterday + "<br>")
+print("--------------------------------" + "<br>")
 
-        for row in reader:
-            RSS_URL = row[1]
-            blog_RSS = feedparser.parse(RSS_URL)
+with open('sites.csv', 'r') as file:
+    reader = csv.reader(file)
 
-            for entry in blog_RSS.entries:
-                if dt_yesterday == entry.date[0:10]:
-                    print("<a href=\"" + entry.link + "\">" + entry.title + "</a>" + ":" + row[0] + "<br>", file=f)
-                    print("...................." + "<br>", file=f)
+    for row in reader:
+        RSS_URL = row[1]
+        blog_RSS = feedparser.parse(RSS_URL)
+
+        for entry in blog_RSS.entries:
+            if dt_yesterday == entry.date[0:10]:
+                print("<a href=\"" + entry.link + "\">" + entry.title + "</a>" + ":" + row[0] + "<br>")
+                print("...................." + "<br>")
